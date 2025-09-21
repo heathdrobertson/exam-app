a36177581d1a42897e04c31a5fd0d8cf89a7ac64
Heath Robertson <heath.d.robertson@gmail.com>
2025-09-21 08:09:45 -0600

    Why?:Replaced both urls for logout
    What these changes address:

--- exam_app/urls.py
+++ exam_app/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
-# path('logout/', auth_views.LogoutView.as_view(next_page='/exams/'), name='logout'),  # Use direct path instead of name
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -24,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('exams/', include('exams.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='exams/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/exams/'), name='logout'),  # Use direct path instead of name
 ]
